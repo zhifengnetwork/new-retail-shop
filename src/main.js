a6ae9fc8f1f3225b9d3b2ba26137f1c60b09f3d4
webhzp <1225613448@qq.com>
2019-06-25 21:37:36 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 配置移动端自适配:安装lib-flexible、安装postcss-px2rem-exclude
+import 'lib-flexible/flexible.js'
+
+// 导入vant所有组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
+// 引入axios请求数据
+import axios from 'axios'
+Vue.prototype.$axios = axios
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
