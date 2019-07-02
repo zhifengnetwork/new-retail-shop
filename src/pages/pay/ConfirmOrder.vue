d3399dacecd0f221b7137599105a65100a550847
zyq <820526949@qq.com>
2019-07-02 19:06:17 +0800

    ssw

--- src/pages/pay/ConfirmOrder.vue
+++ src/pages/pay/ConfirmOrder.vue
@@ -121,169 +121,170 @@ export default {
 </script>
 
 <style lang="stylus">
-    .colorRed
-        color:#f70a0a
-    .size-20
-        font-size:20px
-    .size-36
-        font-size:36px
-    .placehor
-        font-size 18px
-        color:#999
-    .mr-44
-        margin-right :44px
-    .mb-10
-        margin-bottom:10px
-    .content
-        padding:24px
-        color:#151515
-        .user-info-wrap
-            position:relative
-            padding:20px 20px 25px
-            background: #fff
-            border-radius:10px
-            .user-info
-                display:flex
-                align-items :center
-                .icon-ditu
-                    font-size:40px
-                    color: #43c439
-                    margin-right:55px
-                .-info-list
-                    margin-right:20px
-                    .-list-a
-                        color:#151515
-                        font-size:28px
-                        margin-bottom:25px
-                    .-list-b
-                        font-size 24px
-                        color:#555555
-                .-list-edit
-                    margin-left :auto
-                    .iconfont
-                        font-size:30px;
-                        color: #7f7f7f;
-            .-info-img
-                width :94%
-                height:10px
-                position:absolute
-                bottom :0
-        .goods-list      
-            padding 20px
-            border-radius:10px
-            .g-list-a
-                display :flex
-                align-items :center
-                color: #151515;
-                .-list-img
-                    width: 220px
-	                height: 221px
-                .-detial-
-                    margin-left:25px
-                    .-d-msg
-                        font-size 28px
-                        font-weight:bold
-                        margin-bottom :30px
-                        oeverflow:hidden
-                    .-d-msg2
-                        font-size 24px
-                    
-            .g-list-b
-                margin-top:25px
-                .-list-1
+    .confrim-order
+        .colorRed
+            color:#f70a0a
+        .size-20
+            font-size:20px
+        .size-36
+            font-size:36px
+        .placehor
+            font-size 18px
+            color:#999
+        .mr-44
+            margin-right :44px
+        .mb-10
+            margin-bottom:10px
+        .content
+            padding:24px
+            color:#151515
+            .user-info-wrap
+                position:relative
+                padding:20px 20px 25px
+                background: #fff
+                border-radius:10px
+                .user-info
+                    display:flex
+                    align-items :center
+                    .icon-ditu
+                        font-size:40px
+                        color: #43c439
+                        margin-right:55px
+                    .-info-list
+                        margin-right:20px
+                        .-list-a
+                            color:#151515
+                            font-size:28px
+                            margin-bottom:25px
+                        .-list-b
+                            font-size 24px
+                            color:#555555
+                    .-list-edit
+                        margin-left :auto
+                        .iconfont
+                            font-size:30px;
+                            color: #7f7f7f;
+                .-info-img
+                    width :94%
+                    height:10px
+                    position:absolute
+                    bottom :0
+            .goods-list      
+                padding 20px
+                border-radius:10px
+                .g-list-a
                     display :flex
                     align-items :center
-                    margin-bottom:50px
-                    .-b-subtitle
-                        margin:0 25px 0 138px
+                    color: #151515;
+                    .-list-img
+                        width: 220px
+                        height: 221px
+                    .-detial-
+                        margin-left:25px
+                        .-d-msg
+                            font-size 28px
+                            font-weight:bold
+                            margin-bottom :30px
+                            oeverflow:hidden
+                        .-d-msg2
+                            font-size 24px
+                        
+                .g-list-b
+                    margin-top:25px
+                    .-list-1
+                        display :flex
+                        align-items :center
+                        margin-bottom:50px
+                        .-b-subtitle
+                            margin:0 25px 0 138px
+                            font-size:26px
+                        .-option-
+                            border: 2px solid #e6e6e6;
+                            width: 200px;
+                            height: 40px;
+                            line-height :40px;
+                            display :flex;
+                            align-items :center;
+                            justify-content:space-between;
+                            border-radius: 20px;
+                            text-align:center;
+                            margin-left:115px
+                            .iconfont
+                                width:41px;
+                                height:100%;
+                                font-size: 12px;
+                            .puls
+                                border-left:1px solid #e6e6e6;
+                            .subling
+                                border-right:1px solid #e6e6e6;
+                                
+                            .inp
+                                width:121px;
+                                text-align: center;
+                                height:inherit;
+                                font-size:24px;
+                                font-weight:bold;
+                        .-b-msg
+                            color:#999
+                            margin-right:40px
+                    .goods-price
                         font-size:26px
-                    .-option-
-                        border: 2px solid #e6e6e6;
-                        width: 200px;
-                        height: 40px;
-                        line-height :40px;
-                        display :flex;
-                        align-items :center;
-                        justify-content:space-between;
-                        border-radius: 20px;
-                        text-align:center;
-                        margin-left:115px
-                        .iconfont
-                            width:41px;
-                            height:100%;
-                            font-size: 12px;
-                        .puls
-                            border-left:1px solid #e6e6e6;
-                        .subling
-                            border-right:1px solid #e6e6e6;
-                            
-                        .inp
-                            width:121px;
-                            text-align: center;
-                            height:inherit;
-                            font-size:24px;
-                            font-weight:bold;
-                    .-b-msg
-                        color:#999
-                        margin-right:40px
-                .goods-price
-                    font-size:26px
-                    text-align:center
-                    span:nth-child(1)
-                        color:#9d9d9d
-                    span:nth-child(3)
-                        font-size: 30px;
-                        color: #ff112f;
-        .goods-list2
-            margin-top:25px
-            display:flex
-            align-items :center
-            justify-content:space-between
-            font-size:30px
-        .-list2-msg
-            color:#757575
-            margin-top:25px
-        .van-checkbox__icon .van-icon
-            border-color:#434343
-        .van-checkbox__icon--checked .van-icon 
-            background-color: #ff7800;
-            border-color: #ff7800;
+                        text-align:center
+                        span:nth-child(1)
+                            color:#9d9d9d
+                        span:nth-child(3)
+                            font-size: 30px;
+                            color: #ff112f;
+            .goods-list2
+                margin-top:25px
+                display:flex
+                align-items :center
+                justify-content:space-between
+                font-size:30px
+            .-list2-msg
+                color:#757575
+                margin-top:25px
+            .van-checkbox__icon .van-icon
+                border-color:#434343
+            .van-checkbox__icon--checked .van-icon 
+                background-color: #ff7800;
+                border-color: #ff7800;
 
-    .footer-height
-        width :100%;
-        height:140px;
-    .footer
-        position :fixed;
-        bottom:0;
-        width :100%;
-        height:120px;
-        left:0;
-        background:#fff;
-        z-index :2;
-        box-shadow: 0px 0px 5px 0px #cccccc;
-        display :flex;
-        align-items:center
-        .footer-a
-            width:530px;
-            padding-left:28px;
-            display:flex;
-            .f-a-a
+        .footer-height
+            width :100%;
+            height:140px;
+        .footer
+            position :fixed;
+            bottom:0;
+            width :100%;
+            height:120px;
+            left:0;
+            background:#fff;
+            z-index :2;
+            box-shadow: 0px 0px 5px 0px #cccccc;
+            display :flex;
+            align-items:center
+            .footer-a
+                width:530px;
+                padding-left:28px;
                 display:flex;
-                align-items:center;
-                margin-right: 40px;
-            .f-a-b
-                .-freight
-                    margin:0 35px
-                    font-size 24px
-                    color: #757575;
-        .footer-b
-            width:221px;
-            background:#f30c0c;
-            color:#fff;
-            line-height:120px;
-            text-align: center;
-            font-size:30px;
-            font-weight:bold;
+                .f-a-a
+                    display:flex;
+                    align-items:center;
+                    margin-right: 40px;
+                .f-a-b
+                    .-freight
+                        margin:0 35px
+                        font-size 24px
+                        color: #757575;
+            .footer-b
+                width:221px;
+                background:#f30c0c;
+                color:#fff;
+                line-height:120px;
+                text-align: center;
+                font-size:30px;
+                font-weight:bold;
          
 </style>
 
--- src/pages/sell/Sell.vue
+++ src/pages/sell/Sell.vue
@@ -34,8 +34,6 @@
 </template>
 <script>
     import TopHeader from "@/pages/common/header/TopHeader";
-    // import { List } from 'vant';
-    // import { Toast } from 'vant';
     export default {
         data(){
             return {
@@ -162,111 +160,111 @@
 <style lang="stylus">
     .sell
         .van-checkbox__icon
-            height:60px
+            height 60px
         .van-checkbox__icon .van-icon
-            width:60px
-            height:60px
-            line-height:60px
-            border: 1px solid #151515;
-            background: #fff;
+            width 60px
+            height 60px
+            line-height 60px
+            border 1px solid #151515
+            background #fff
         .van-checkbox__icon--checked .van-icon
-            background-color: #f30c0c;
-            border-color: #fff;
-            border: none;
+            background-color #f30c0c
+            border-color #fff
+            border none
         .right-msg
-            font-size:26px
+            font-size 26px
         .content
-            padding:24px 24px
+            padding 24px 24px
             .sell-list
-                display:block
-                width:100%
-                border-radius: 10px
+                display block
+                width 100%
+                border-radius 10px
                 position relative
                 .-list-img
-                    width:100%
-                    max-height :100%
+                    width 100%
+                    max-height 100%
                 .-list-msg
                     position absolute
-                    top:20px
-                    left :25px 
-                    font-size:26px
-                    color:#fff
-                    width :180px
-                    text-align: center
-                    background: rgba(0,0,0,0.3)
-                    padding: 6px 3px
-                    box-sizing: border-box
-                    border-radius: 8px
+                    top 20px
+                    left 25px 
+                    font-size 26px
+                    color #fff
+                    width 180px
+                    text-align center
+                    background rgba(0,0,0,0.3)
+                    padding 6px 3px
+                    box-sizing border-box
+                    border-radius 8px
                     p:nth-child(1)
-                        margin-bottom:4px
+                        margin-bottom 4px
                         .msg-a
-                            background: red
-                            padding: 5px
-                            width: 21px
-                            text-align: center
-                            border-radius: 3px
+                            background red
+                            padding 5px
+                            width 21px
+                            text-align center
+                            border-radius 3px
                 .-list-radus-num
-                    position: absolute
-                    top: 0.24rem
-                    right: 0
-                    width: 100px
-                    height: 60px
-                    line-height :60px
-                    background: #e5233e
-                    color: #fff
-                    border-bottom-left-radius:30px
-                    border-top-left-radius:30px
-                    text-align :center
+                    position absolute
+                    top 0.24rem
+                    right 0
+                    width 100px
+                    height 60px
+                    line-height 60px
+                    background #e5233e
+                    color #fff
+                    border-bottom-left-radius 30px
+                    border-top-left-radius 30px
+                    text-align center
                 .-list-radus-box
-                    position :absolute
-                    right:20px
-                    bottom :22px
-                    color:#f30c0c
-                    width :60px
-                    height:60px
+                    position absolute
+                    right 20px
+                    bottom 22px
+                    color #f30c0c
+                    width 60px
+                    height 60px
                     .-radus-1
-                        display:block
-                        width :50px
+                        display block
+                        width 50px
                         height 50px
-                        line-height:50px
-                        text-align:center
-                        font-size:26px
-                        border-radius:50%
-                        background: #fff
-                        box-sizing: border-box
-                        border:1px solid #000
+                        line-height 50px
+                        text-align center
+                        font-size 26px
+                        border-radius 50%
+                        background #fff
+                        box-sizing border-box
+                        border 1px solid #000
                     .-radus-check
-                        background: #f30c0c
-                        border-radius: 50%
-                        color: #fff
-                        padding: 5px
-                        box-sizing: border-box
+                        background #f30c0c
+                        border-radius 50%
+                        color #fff
+                        padding 5px
+                        box-sizing border-box
         .footer-height
-            width :100%
-            height:130px
+            width 100%
+            height 130px
         .footer
-            position :fixed
-            bottom:0
-            width :100%
-            height:120px
-            left:0
-            background:#fff
-            z-index :2
-            box-shadow: 0px 0px 5px 0px #cccccc
-            display :flex
-            justify-content :space-between
-            align-items:center
-            font-size:30px
-            font-weight:bold
+            position fixed
+            bottom 0
+            width  100%
+            height 120px
+            left 0
+            background #fff
+            z-index 2
+            box-shadow 0px 0px 5px 0px #cccccc
+            display flex
+            justify-content space-between
+            align-items center
+            font-size 30px
+            font-weight bold
             .f-a-a
-                margin:0 96px 0 24px
+                margin 0 96px 0 24px
             .footer-b
-                width:221px
-                background:#858585
-                color:#fff
-                line-height:120px
-                text-align: center
+                width 221px
+                background #858585
+                color #fff
+                line-height 120px
+                text-align center
             .activeColor
-                background:#f30c0c
+                background #f30c0c
 </style>
 
