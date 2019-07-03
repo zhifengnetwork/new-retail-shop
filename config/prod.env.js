aba565a7299ce37ab8585a9e6e487eeed27d3dab
ICEY-I-SEE <1141839113@qq.com>
2019-07-03 10:42:26 +0800

    dd

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
-module.exports = {
-  NODE_ENV: '"production"'
-}
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"production"',
+  API_ROOT: '"https://jifen.zhifengwangluo.com/api"'   //线上请求前缀
+})
