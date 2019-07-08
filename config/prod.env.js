6a851228da5eb5aa6c1e487169b7664d07432d67
zhangguiping <270081986@qq.com>
2019-07-08 20:38:37 +0800

    修改正式环境可以访问http://new_retail_web.zhifengwangluo.com

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  API_ROOT: '"http://new_retail_api.zhifengwangluo.com"'   //线上请求前缀
+  API_ROOT: '"http://new_retail_web.zhifengwangluo.com/api/"'   //线上请求前缀
 })
