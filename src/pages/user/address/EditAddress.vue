564d8e1322f3f9a552b449d5a828dcfeafac0cff
chenguangjie111 <979230865@qq.com>
2019-07-10 09:29:41 +0800

    地址
    地址

--- src/pages/user/address/EditAddress.vue
+++ src/pages/user/address/EditAddress.vue
@@ -87,6 +87,7 @@ export default {
             _that.detailAddress =_that.$refs.detailAddress.innerText
             var url ='/address/addAddress'
             if(!_that._verifyUserInfo()){return}
+          
             if(!_that.district){_that.$toast("请选择省市区");return}
             _that.$axios.post(url,{
                 // 传给后台的参数
