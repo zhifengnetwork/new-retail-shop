3793b1d174a1ce4e887f9918e22ed6e736aab226
zyq <820526949@qq.com>
2019-07-16 15:34:08 +0800

    www

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  API_ROOT: '"http://newretailweb.zhifengwangluo.com/api/"'   //线上请求前缀
+  API_ROOT: '"http://newretailwebtest.zhifengwangluo.com/api/"'   //线上请求前缀
 })
