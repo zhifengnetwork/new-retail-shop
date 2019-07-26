82314a48e0abb2c4266d0b700fb8e1c708bec446
zyq <820526949@qq.com>
2019-07-26 14:40:31 +0800

    wss

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
   // API_ROOT: '"http://newretailweb.zhifengwangluo.com/api/"'   //线上请求前缀 测试
-  API_ROOT: '"http://www.hmylst.cn/api/"'   //线上请求前缀
-  // API_ROOT: '"http://www.puruitingxls.com/api/"'   //线上请求前缀
+  // API_ROOT: '"http://www.hmylst.cn/api/"'   //线上请求前缀
+  API_ROOT: '"http://www.puruitingxls.com/api/"'   //线上请求前缀
 })
