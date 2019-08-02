b60cccb0e37e40216da17272086ea25234af543f
zyq <820526949@qq.com>
2019-08-02 20:56:39 +0800

    se

--- src/pages/sell/Sell.vue
+++ src/pages/sell/Sell.vue
@@ -19,8 +19,15 @@
                     <img class="-list-img" :src="item.img" @click="showPopup(item.pay_code)"/>
                     <div class="-list-radus-num">{{item.stock}}</div>
                 </div>
-                <div class="-list-radus-box">
-                    <van-checkbox v-model="item.isCheck" :check ="item.isCheck"></van-checkbox>
+                <div class="-list-right">
+                    <!-- <div class="-list-radus-box">
+                        <van-checkbox v-model="item.isCheck" :check ="item.isCheck"></van-checkbox>
+                    </div> -->
+                    <span class="-option-">
+                        <i class="subling iconfont icon-jian-" @click="reducingNumber(key,item.fz_id)"></i>
+                        <input class="inp"  disabled type="number" :value="item.goods_num" @change="changNumber($event,key,item.fz_id)"/>
+                        <i class="puls iconfont icon-jia" @click="addNumber(key,item.fz_id)" ></i>
+                    </span>
                 </div>
             </a>
         </div>
@@ -28,10 +35,10 @@
         <div class="footer">
             <div class="footer-a">
                 <strong class="f-a-a">
-                    已选商品: {{totalCount}}
+                    已选商品: {{numArray.length}}
                 </strong>
             </div>
-            <div class="footer-b" :class="{'activeColor':totalCount==10}" @click="toPayment">付款</div>
+            <div class="footer-b" :class="{'activeColor':numArray.length==10}" @click="toPayment">付款</div>
         </div>
         <!-- 弹出层 -->
         <van-popup v-model="show"><img :src="shopsCode" /></van-popup>
@@ -50,7 +57,8 @@
                 show: false,
                 isShow:false,//弹窗是否显示
                 iswoo:false,
-                token:this.$store.getters.optuser.Authorization
+                token:this.$store.getters.optuser.Authorization,
+                numArray:[]    //存放选中的数量
             }
         },
         created(){
@@ -58,10 +66,60 @@
             this._getSellList()
         },
         methods:{
+            computedNum(){
+                var info =this.numArray
+                var t =0
+                for(var i in info){
+                    t =t+info[i]
+                    if(t>=10){
+                         this.$toast.fail('限制只能购买10个');
+                         return false
+                    }
+                }
+                return true
+            },
+
+            reducingNumber(key,sku_id){     //减数量
+                var _that =this,
+                    data =_that.list[key],
+                    val =parseInt(data.goods_num - 1),
+                    oldVal =val
+                if(val<=0){
+                    val =0
+                }
+                if(data.goods_num!=0){  //有数量的前提下，存放数量的数组减1
+                    var newarr=_that.numArray.slice(0);
+                    newarr.shift();
+                     _that.numArray =newarr
+                }
+                var data =_that.list[key];
+                _that.$set( data,'goods_num',val)
+            },
+            addNumber(key,sku_id){
+                if(!this.computedNum()){return}
+                var _that =this,
+                    data =_that.list[key],
+                    val =parseInt(data.goods_num), 
+                    val =new Number(val+ 1)
+                if(val>9){
+                    _that.$set(data,'goods_num',9)
+                    this.$forceUpdate();
+                    return _that.$toast('该商品限购9件');
+                }else{
+                    if(val>data.stock){
+                        _that.$set(data,'goods_num',data.stock)
+                        this.$forceUpdate();
+                        return _that.$toast('该商品库存为:'+data.stock+'件');
+                    }
+                }
+                var data =_that.list[key];
+                _that.$set( data,'goods_num',val)
+                _that.numArray.push(1)          //数量加1 存放数量的数组插入一个1 用于占位
+            },
             toPay(){
                 this.$router.push('/Payment')
             },
-            _getSellList(){
+            _getSellList(){         //获取商品信息
                 var _that =this
                 _that.$axios.post('fifty_zone/shop_list',{
                     'token':this.token,   
@@ -69,7 +127,11 @@
                 .then((res)=>{
                     var list = res.data;
                     if(list.status == 200){
-                        _that.list =list.data
+                        var info =list.data
+                        for(var i in info){         //往数组里面追加一个数量默认为0
+                            info[i].goods_num =0
+                        }
+                        _that.list =info
                         _that.$store.commit('hideLoading')
                     }else if(list.status == 304){
                         _that.$toast(list.msg)
@@ -93,27 +155,29 @@
                 this.isShow = false
             },
             toPayment(){
-                var _that =this,fzids='';
-                if(_that.totalCount != 10){
-                    return _that.$toast.fail('限制只能购买10个');
+                if(this.numArray.length!=10){
+                    return this.$toast.fail('限制只能购买10个')
                 }
+                var _that =this,fzids='';
                 for(var i in _that.list){
-                    if(_that.list[i].isCheck){
-                        if(fzids==""){
-                            fzids =_that.list[i].fz_id
-                        }else{
-                            fzids = fzids+','+_that.list[i].fz_id
+                    var goods_num =_that.list[i].goods_num
+                    if(goods_num>0){
+                        for(var j=0; j<goods_num; j++){
+                            if(fzids==""){
+                                fzids =_that.list[i].fz_id
+                            }else{
+                                fzids = fzids+','+_that.list[i].fz_id
+                            }
                         }
-                       
                     }
                 }
+                console.log(fzids)
                 _that.$axios.post('fifty_zone/fiftySubmitOrder',{
                     'token':_that.token,
                     'fz_id':fzids    
                 })
                 .then((res)=>{
                     var list = res.data;
-                    console.log(res)
                     if(list.status == 200){
                         this.$router.push({name:'Payment'})
                     }
@@ -151,15 +215,10 @@
             showPopup(paycode) {
                 this.show = true;
                 this.shopsCode =paycode
-            },
-            // confirmOrder(){
-            //     if(this.totalCount != 10){
-            //         return this.$toast.fail('只能购买10个');
-            //     }
-            //     this.$router.push({name:'Payment'})
-            // }
+            }
         },
         computed:{
+
             totalCount(){
                 let count = 0;
                 this.list.forEach((data)=>{
@@ -169,6 +228,7 @@
                 })
                 return count;
             }
+            
         },
         components:{
             TopHeader,
@@ -198,11 +258,14 @@
                 display block
                 width 100%
                 border-radius 10px
-                position relative
+                display flex
+                align-items center
+                justify-content space-between
                 .sell-cont
                     width 450px
                     height 280px
                     margin-bottom 15px
+                    position relative
                     .-list-img
                         width 100%
                         height 100%
@@ -239,31 +302,58 @@
                         border-bottom-right-radius 30px
                         border-top-right-radius 30px
                         text-align center
-                .-list-radus-box
-                    position absolute
-                    right 20px
-                    top 50%
-                    margin-top -30px
-                    color #f30c0c
-                    width 60px
-                    height 60px
-                    .-radus-1
-                        display block
-                        width 50px
-                        height 50px
-                        line-height 50px
+                .-list-right
+                    display flex
+                    flex-wrap wrap
+                    justify-content flex-end
+                    .-list-radus-box
+                        color #f30c0c
+                        width 60px
+                        height 60px
+                        .-radus-1
+                            display block
+                            width 50px
+                            height 50px
+                            line-height 50px
+                            text-align center
+                            font-size 26px
+                            border-radius 50%
+                            background #fff
+                            box-sizing border-box
+                            border 1px solid #000
+                        .-radus-check
+                            background #f30c0c
+                            border-radius 50%
+                            color #fff
+                            padding 5px
+                            box-sizing border-box
+                    .-option-
+                        border 2px solid #ccc
+                        width 200px
+                        height 48px
+                        line-height 48px
+                        display flex
+                        align-items center
+                        justify-content space-between
+                        border-radius 20px
                         text-align center
-                        font-size 26px
-                        border-radius 50%
-                        background #fff
-                        box-sizing border-box
-                        border 1px solid #000
-                    .-radus-check
-                        background #f30c0c
-                        border-radius 50%
-                        color #fff
-                        padding 5px
-                        box-sizing border-box
+                        margin-top 30px
+                        color:#151515
+                        .iconfont
+                            width 100px
+                            height 100%
+                            font-size 30px
+                        .puls
+                            border-left 1px solid #ccc
+                        .subling
+                            border-right 1px solid #ccc
+                            
+                        .inp
+                            width 100px
+                            text-align center
+                            height inherit
+                            font-size 28px
+                            font-weight bold
         .footer-height
             width 100%
             height 130px
